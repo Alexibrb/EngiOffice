f8b6a458c4f81ec041767f04a92189da45bbadc8
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-08-15 12:02:53 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALI2S7-ja1Gcfaa5Kgmz9iMKrC1Fqagyo",
+  authDomain: "engiflow-74dlq.firebaseapp.com",
+  projectId: "engiflow-74dlq",
+  storageBucket: "engiflow-74dlq.appspot.com",
+  messagingSenderId: "700766733055",
+  appId: "1:700766733055:web:c831392d6fd34491b142ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
