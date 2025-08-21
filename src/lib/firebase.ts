77ea352546ea9af84b15c280673906961fd0ebf7
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2025-08-21 13:52:26 +0000

    tenho mais uma dúvida com relação ao banco de dados, o banco de dados at

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,16 +8,16 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "engiflow-74dlq",
-  "appId": "1:700766733055:web:1e4d597364d4810cb142ca",
-  "storageBucket": "engiflow-74dlq.firebasestorage.app",
-  "apiKey": "AIzaSyALI2S7-ja1Gcfaa5Kgmz9iMKrC1Fqagyo",
-  "authDomain": "engiflow-74dlq.firebaseapp.com",
-  "messagingSenderId": "700766733055"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 
